aab684aabdd872c7fa9c5b29918c5ca303cd9508
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-08-30 08:30:59 +0700

    Init

---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/HashBenchmarks.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using CryptographyHelper;
+using CryptographyHelper.HashAlgorithms;
+
+namespace Practical.BenchmarkDotNet
+{
+    public class HashBenchmarks
+    {
+        [Benchmark]
+        public void Md5()
+        {
+            const string originalMessage = "Original Message to hash";
+            var hashed = originalMessage.UseMd5().ComputeHash().ToHashedString();
+        }
+
+        [Benchmark]
+        public void Sha1()
+        {
+            const string originalMessage = "Original Message to hash";
+            var hashed = originalMessage.UseSha1().ComputeHash().ToHashedString();
+        }
+
+        [Benchmark]
+        public void Sha256()
+        {
+            const string originalMessage = "Original Message to hash";
+            var hashed = originalMessage.UseSha256().ComputeHash().ToHashedString();
+        }
+
+        [Benchmark]
+        public void Sha384()
+        {
+            const string originalMessage = "Original Message to hash";
+            var hashed = originalMessage.UseSha384().ComputeHash().ToHashedString();
+        }
+
+        [Benchmark]
+        public void Sha512()
+        {
+            const string originalMessage = "Original Message to hash";
+            var hashed = originalMessage.UseSha512().ComputeHash().ToHashedString();
+        }
+    }
+}
---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Practical.BenchmarkDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<HashBenchmarks>();
+        }
+    }
+}
