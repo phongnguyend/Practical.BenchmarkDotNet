3cd43eef66aeb7de74218a0ca16690066e7d38fe
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-05-31 16:45:53 +0700

    Dictionary Lookup Vs Pattern Matching Benchmarks

---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/DictionaryLookupVsPatternMatchingBenchmarks.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Practical.BenchmarkDotNet;
+
+[MemoryDiagnoser]
+public class DictionaryLookupVsPatternMatchingBenchmarks
+{
+    private static readonly Dictionary<string, string> _sharedDic = new()
+    {
+        { "VN", "Viet Nam" },
+        { "US", "United States" },
+        { "GB", "Great Britain" }
+    };
+
+    [Benchmark]
+    public void DictionaryLookup()
+    {
+        var dic = new Dictionary<string, string>
+        {
+            { "VN", "Viet Nam" },
+            { "US", "United States" },
+            { "GB", "Great Britain" }
+        };
+
+        var country = dic["US"];
+    }
+
+    [Benchmark]
+    public void SharedDictionaryLookup()
+    {
+        var country = _sharedDic["US"];
+    }
+
+    [Benchmark]
+    public void PatternMatching()
+    {
+        var country = GetCountry("US");
+    }
+
+    private static string GetCountry(string code)
+    {
+        return code switch
+        {
+            "VN" => "Viet Nam",
+            "US" => "United States",
+            "GB" => "Great Britain",
+            _ => throw new ArgumentException("Invalid Code", nameof(code)),
+        };
+    }
+}
