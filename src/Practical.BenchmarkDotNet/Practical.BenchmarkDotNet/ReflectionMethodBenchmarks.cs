22ce6f1f98a3e7789356edea7d35aa07bd225528
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-06-01 10:56:39 +0700

    Reflection Benchmarks

---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/ReflectionMethodBenchmarks.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Reflection;
+
+namespace Practical.BenchmarkDotNet;
+
+[MemoryDiagnoser]
+public class ReflectionMethodBenchmarks
+{
+    private readonly Person _person = new Person { Id = 1 };
+    private static readonly MethodInfo _methodInfo = typeof(Person).GetMethod("SomeMethod");
+    private static readonly Func<Person, string, string> _methodDelegate = (Func<Person, string, string>)Delegate.CreateDelegate(typeof(Func<Person, string, string>), _methodInfo);
+
+    [Benchmark]
+    public void SomeMethod()
+    {
+        var rs = _person.SomeMethod("");
+    }
+
+    [Benchmark]
+    public void SomeMethodReflection()
+    {
+        var rs = typeof(Person).GetMethod("SomeMethod").Invoke(_person, [""]);
+    }
+
+    [Benchmark]
+    public void SomeMethodReflectionCached()
+    {
+        var rs = _methodInfo.Invoke(_person, [""]);
+    }
+
+    [Benchmark]
+    public void SomeMethodDelegate()
+    {
+        var rs = _methodDelegate(_person, "");
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string SomeMethod(string name)
+        {
+            return name;
+        }
+    }
+}
---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/ReflectionPropertyBenchmarks.cs
@@ -0,0 +1,71 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Reflection;
+
+namespace Practical.BenchmarkDotNet;
+
+[MemoryDiagnoser]
+public class ReflectionPropertyBenchmarks
+{
+    private readonly Person _person = new Person { Id = 1 };
+    private static readonly PropertyInfo _propertyInfo = typeof(Person).GetProperty("Id");
+    private static readonly Func<Person, int> _getIdDelegate = (Func<Person, int>)Delegate.CreateDelegate(typeof(Func<Person, int>), _propertyInfo.GetGetMethod());
+    private static readonly Action<Person, int> _setIdDelegate = (Action<Person, int>)Delegate.CreateDelegate(typeof(Action<Person, int>), _propertyInfo.GetSetMethod());
+
+    [Benchmark]
+    public void GetId()
+    {
+        var id = _person.Id;
+    }
+
+    [Benchmark]
+    public void GetIdReflection()
+    {
+        var id = (int)typeof(Person).GetProperty("Id").GetValue(_person);
+    }
+
+    [Benchmark]
+    public void GetIdReflectionCached()
+    {
+        var id = (int)_propertyInfo.GetValue(_person);
+    }
+
+    [Benchmark]
+    public void GetIdDelegate()
+    {
+        var id = _getIdDelegate(_person);
+    }
+
+    [Benchmark]
+    public void SetId()
+    {
+        _person.Id = 2;
+    }
+
+    [Benchmark]
+    public void SetIdReflection()
+    {
+        typeof(Person).GetProperty("Id").SetValue(_person, 2);
+    }
+
+    [Benchmark]
+    public void SetIdReflectionCached()
+    {
+        _propertyInfo.SetValue(_person, 2);
+    }
+
+    [Benchmark]
+    public void SetIdDelegate()
+    {
+        _setIdDelegate(_person, 2);
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string SomeMethod(string name)
+        {
+            return name;
+        }
+    }
+}
