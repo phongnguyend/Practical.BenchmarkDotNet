fb969bd5712b83d7a3799ff70fb12a75488923cb
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-06-01 21:01:25 +0700

    TryParse and String IsNullOrEmpty Benchmarks

---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/TryParseStringIsNullOrEmptyBenchmarks.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Practical.BenchmarkDotNet;
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class TryParseStringIsNullOrEmptyBenchmarks
+{
+    [Params("123", "abc", null, "", "  ")]
+    public string Input { get; set; }
+
+    [Benchmark]
+    public void TryParse()
+    {
+        if(int.TryParse(Input, out int result))
+        {
+        }    
+    }
+
+    [Benchmark]
+    public void IsNullOrEmpty()
+    {
+        if (!string.IsNullOrEmpty(Input) && int.TryParse(Input, out int result))
+        {
+        }
+    }
+
+    [Benchmark]
+    public void IsNullOrWhiteSpace()
+    {
+        if (!string.IsNullOrWhiteSpace(Input) && int.TryParse(Input, out int result))
+        {
+        }
+    }
+}
