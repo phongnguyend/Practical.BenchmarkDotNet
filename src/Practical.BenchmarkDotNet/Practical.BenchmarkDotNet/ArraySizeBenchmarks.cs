7394f11af9799327f29afe9fe9068571b641f6bf
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-06-02 22:00:27 +0700

    Array Size and List Size Benchmarks

---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/ArraySizeBenchmarks.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Practical.BenchmarkDotNet;
+
+[WarmupCount(0)]
+[IterationCount(1)]
+[InvocationCount(1)]
+[MemoryDiagnoser]
+public class ArraySizeBenchmarks
+{
+    [Benchmark]
+    public void Bool()
+    {
+        var x = new bool[Array.MaxLength / 2];
+    }
+
+    [Benchmark]
+    public void Int()
+    {
+        var x = new int[Array.MaxLength / 2];
+    }
+
+    [Benchmark]
+    public void Float()
+    {
+        var x = new float[Array.MaxLength / 2];
+    }
+
+    [Benchmark]
+    public void Long()
+    {
+        var x = new long[Array.MaxLength / 2];
+    }
+
+    [Benchmark]
+    public void Decimal()
+    {
+        var x = new decimal[Array.MaxLength / 2];
+    }
+
+    [Benchmark]
+    public void String()
+    {
+        var x = new string[Array.MaxLength / 2];
+    }
+
+    [Benchmark]
+    public void Struct()
+    {
+        var x = new PersonStruct[Array.MaxLength / 2];
+    }
+
+    [Benchmark]
+    public void Class()
+    {
+        var x = new PersonClass[Array.MaxLength / 2];
+    }
+
+    private struct PersonStruct
+    {
+        public int Int { get; set; }
+
+        public decimal Decimal { get; set; }
+    }
+
+    private class PersonClass
+    {
+        public int Int { get; set; }
+
+        public decimal Decimal { get; set; }
+    }
+}
---
+++ src/Practical.BenchmarkDotNet/Practical.BenchmarkDotNet/ListSizeBenchmarks.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Practical.BenchmarkDotNet;
+
+[WarmupCount(0)]
+[IterationCount(1)]
+[InvocationCount(1)]
+[MemoryDiagnoser]
+public class ListSizeBenchmarks
+{
+    [Benchmark]
+    public void Bool()
+    {
+        var x = new List<bool>(Array.MaxLength / 2);
+    }
+
+    [Benchmark]
+    public void Int()
+    {
+        var x = new List<int>(Array.MaxLength / 2);
+    }
+
+    [Benchmark]
+    public void Float()
+    {
+        var x = new List<float>(Array.MaxLength / 2);
+    }
+
+    [Benchmark]
+    public void Long()
+    {
+        var x = new List<long>(Array.MaxLength / 2);
+    }
+
+    [Benchmark]
+    public void Decimal()
+    {
+        var x = new List<decimal>(Array.MaxLength / 2);
+    }
+
+    [Benchmark]
+    public void String()
+    {
+        var x = new List<string>(Array.MaxLength / 2);
+    }
+
+    [Benchmark]
+    public void Struct()
+    {
+        var x = new List<PersonStruct>(Array.MaxLength / 2);
+    }
+
+    [Benchmark]
+    public void Class()
+    {
+        var x = new List<PersonClass>(Array.MaxLength / 2);
+    }
+
+    private struct PersonStruct
+    {
+        public int Int { get; set; }
+
+        public decimal Decimal { get; set; }
+    }
+
+    private class PersonClass
+    {
+        public int Int { get; set; }
+
+        public decimal Decimal { get; set; }
+    }
+}
